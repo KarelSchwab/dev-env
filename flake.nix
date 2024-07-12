da10c81620662449f48144f554212c58b68d0498
Karel Schwab <35557567+KarelSchwab@users.noreply.github.com>
2024-07-12 20:45:34 +0200

    Update .gitignore

--- flake.nix
+++ flake.nix
@@ -4,35 +4,58 @@
   # Flake inputs
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+      pkgs = import nixpkgs { inherit system; };
     in
     {
       # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = with pkgs; [
-            python311Full
-            python311Packages.autopep8
-            python311Packages.build
-          ];
+      devShell = pkgs.mkShell {
+        # The Nix packages provided in the environment
+        packages = with pkgs; [
+          python311Full
+          python311Packages.autopep8
+          python311Packages.build
+        ];
+      };
+
+      packages.default = pkgs.python3Packages.buildPythonApplication {
+        pname = "dev-env";
+        version = "0.0.1";
+        src = ./.;
+
+        buildInputs = [ pkgs.python3 ];
+
+        doCheck = false;
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ${./src/main.py} $out/bin/main.py
+          chmod +x $out/bin/main.py
+        '';
+
+        meta = with pkgs.lib; {
+          description = "A Nix flake project generator";
+          license = licenses.mit;
+          # maintainers = with maintainers; [ your-github-username ];
         };
-      });
-    };
+      };
+    });
 }
+      # Development environment output
+      # devShells = forAllSystems ({ pkgs }: {
+      #   default = pkgs.mkShell {
+      #     # The Nix packages provided in the environment
+      #     packages = with pkgs; [
+      #       python311Full
+      #       python311Packages.autopep8
+      #       python311Packages.build
+      #     ];
+      #   };
+      # });
+    # }
