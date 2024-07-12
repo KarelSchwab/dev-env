364509ce6ee28f348da0db97e797134a40405fd5
Karel Schwab <35557567+KarelSchwab@users.noreply.github.com>
2024-07-12 22:04:19 +0200

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -11,37 +11,38 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      # Development environment output
-      devShell = pkgs.mkShell {
-        # The Nix packages provided in the environment
-        packages = with pkgs; [
-          python311Full
-          python311Packages.autopep8
-        ];
-      };
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # Development environment output
+        devShell = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            python311Full
+            python311Packages.autopep8
+          ];
+        };
 
-      packages.default = pkgs.python3Packages.buildPythonApplication {
-        pname = "dev-env";
-        version = "0.0.1";
-        pyproject = true;
-        src = ./.;
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          pname = "dev-env";
+          version = "0.0.1";
+          pyproject = true;
+          src = ./.;
 
-        buildInputs = [ 
-          pkgs.python3
-          pkgs.python3Packages.hatchling
-        ];
+          buildInputs = [
+            pkgs.python3
+            pkgs.python3Packages.hatchling
+          ];
 
-        doCheck = false;
+          doCheck = false;
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp ${./src/main.py} $out/bin/dev-env
-          chmod +x $out/bin/dev-env
-        '';
-      };
-    });
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${./src/main.py} $out/bin/dev-env
+            chmod +x $out/bin/dev-env
+          '';
+        };
+      }
+    );
 }
 
